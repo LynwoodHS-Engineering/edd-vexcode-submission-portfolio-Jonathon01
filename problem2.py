8a53076f23a5f5ec8c961c8771f1d174e3f49324
Jonathon01 <jonlop1023@stu.mylusd.org>
2025-02-06 19:07:12 -0800

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -1,3 +1,44 @@
+#region VEXcode Generated Robot Configuration
+from vex import *
+import urandom
+
+# Brain should be defined by default
+brain=Brain()
+
+# Robot configuration code
+limit_switch_a = Limit(brain.three_wire_port.a)
+limit_switch_b = Limit(brain.three_wire_port.b)
+led_g = Led(brain.three_wire_port.g)
+led_h = Led(brain.three_wire_port.h)
+
+
+# wait for rotation sensor to fully initialize
+wait(30, MSEC)
+
+
+# Make random actually random
+def initializeRandomSeed():
+    wait(100, MSEC)
+    random = brain.battery.voltage(MV) + brain.battery.current(CurrentUnits.AMP) * 100 + brain.timer.system_high_res()
+    urandom.seed(int(random))
+      
+# Set random seed 
+initializeRandomSeed()
+
+
+def play_vexcode_sound(sound_name):
+    # Helper to make playing sounds from the V5 in VEXcode easier and
+    # keeps the code cleaner by making it clear what is happening.
+    print("VEXPlaySound:" + sound_name)
+    wait(5, MSEC)
+
+# add a small delay to make sure we don't print in the middle of the REPL header
+wait(200, MSEC)
+# clear the console to make sure we don't have the REPL in the console
+print("\033[2J")
+
+#endregion VEXcode Generated Robot Configuration
+
 from vex import *
 
 brain=Brain()
@@ -19,3 +60,4 @@ while True:
          led_g.off()
          led_h.on()
          limit_switch_a.pressing()
+   
